235baad665ad1d83928144c5cbf0aa09a32bcef0
AdriUlima <20200711@aloe.ulima.edu.pe>
2023-11-28 01:19:12 -0500

    Solicitudes Frontend
    Co-Authored-By: Aron-png <122492786+Aron-png@users.noreply.github.com>

--- Frontend/pages/solicitudes.js
+++ Frontend/pages/solicitudes.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/router";
 import styles from "./solicitudes.module.css";
 import Lateral from "../components/lateral.js";
@@ -7,26 +7,10 @@ import { Zoom } from "../extra/zoom.js";
 import SolicitudApi from "../api/solicitud.js";
 import Global from "../extra/global.js";
 
-/*Recibidas:
-- Cargar del Backend todas las solicitudes donde el usuario es receptor, y que sean de tipo 0 (Solicitud enviada)
-- Cada una tiene 2 botones: Aceptar (UPDATE Solicitud, cambiando Tipo a 2), rechazar (DELETE Solicitud)
-Enviadas:
-- Cargar del Backend todas las solicitudes donde el usuario es emisor, y que sean de tipo 0 (Solicitud enviada)
-- Cada una tiene 2 botones: Ocultar (UPDATE Solicitud, cambiando Tipo a 1), cancelar (DELETE Solicitud)
-Ocultas:
-- Cargar del Backend todas las solicitudes donde el usuario es emisor, y que sean de tipo 1 (Solicitud oculta)
-- Cada una tiene 2 botones: Enviar (UPDATE Solicitud, cambiando Tipo a 0), cancelar (DELETE Solicitud)
-Guardadas: Eliminar
-
+/*
 Por cada solicitud:
-- Utilizar el id del otro usuario para agarrar sus datos del UsuarioApi (nombre, foto, etc)
-- Si la variable mostrar-nombre es true, mostrar el nombre; sino, el apodo
 - Mostrar foto
 - Al darles clic, llevan a http://localhost:3000/perfil?id={} (reemplazar {} por el id del otro usuario)
-
-- Abajo están las variables del objeto Solicitud, cada una explicadas
-- UNIFICAR PANTALLAS, CAMBIAR EL NOMBRE DE ÉSTA A solicitudes, pasar el contenido del resto a ésta y tras eso ELIMINARLOS CON SUS MODULE.CSS
-- Después de hacer cualquier llamado al Backend, volver a recargar los datos (GET)
 */
 
 const Solicitudes = () => {
@@ -74,61 +58,29 @@ const Solicitudes = () => {
     setShowMessage(false);
   };
 
-  useEffect(() => {
-
-    //UsuarioApi.findAll().then((users) => {
-    //const aux = user.data.usuarios;
-    //setUsuarios(aux)
-    setUsuarios([
-      {...defaultUsuario,_id:'0',nombre:'Yo',apodo:'YoApodo',mostrar_nombre:true},
-      {...defaultUsuario,_id:'1',nombre:'Adrián',apodo:'AdriánApodo',mostrar_nombre:true},
-      {...defaultUsuario,_id:'2',nombre:'RodrigoNombre',apodo:'Rodrigo',mostrar_nombre:false},
-      {...defaultUsuario,_id:'3',nombre:'Camayo',apodo:'CamayoApodo',mostrar_nombre:true},
-      {...defaultUsuario,_id:'4',nombre:'George',apodo:'GeorgeApodo',mostrar_nombre:true}
-    ])
-
-    //UsuarioApi.findCurrent().then((user) => {
-    //const aux = user.data.usuario;
-    //setUsuario(aux)
-    setUsuario({...defaultUsuario,_id:'0',nombre:'Yo',apodo:'YoApodo',mostrar_nombre:true})
-    // Utiliza la función SolicitudesRecibidasUsuario de tu API de solicitudes
-    SolicitudApi.SolicitudesRecibidasUsuario(window.localStorage.token)
-      .then((response) => {
-        console.log("resp",response)
-        if (response && response.data) {
-          
-          //const solicitudesData = response.data.solicitudesRecibidas;
-          //setSolicitudes(solicitudesData);
-          setSolicitudes([
-            {...defaultSolicitud,_id:'0',idEmisor:'1',idReceptor:'0',tipo:0, ano:2023,mes:11,dia:27,diaSem:"Lunes",hora:2,minuto:45},
-            {...defaultSolicitud,_id:'1',idEmisor:'2',idReceptor:'0',tipo:0, ano:2023,mes:11,dia:26,diaSem:"Lunes",hora:2,minuto:45},
-            {...defaultSolicitud,_id:'2',idEmisor:'0',idReceptor:'3',tipo:0, ano:2023,mes:11,dia:25,diaSem:"Lunes",hora:2,minuto:45},
-            {...defaultSolicitud,_id:'3',idEmisor:'0',idReceptor:'4',tipo:1, ano:2023,mes:11,dia:10,diaSem:"Lunes",hora:2,minuto:45},
-          ]);
-
-          // Calcular la cantidad de solicitudes sin viewer en 1
-           
-          
-        }
-      })
-      .catch((error) => {
-        console.error("Error al obtener las solicitudes:", error);
-      });
-
-    // Llama a la función para actualizar el campo viewer
-    SolicitudApi.actualizarViewerSolicitudes(window.localStorage.token)
-      .then((response) => {
-        if (response && response.data) {
-          console.log(response.data.message);
-        }
-      })
-      .catch((error) => {
-        console.error("Error al actualizar el viewer:", error);
-      });
-    
-  }, []);
+  const botonAmarillo = (s) => {
+    if(page===1){
+      //SolicitudApi.actualizar
+      console.log({...s,tipo:2})
+    }else if(page===2){
+      //SolicitudApi.borrar(s._id)
+    }else{
+      //SolicitudApi.actualizar
+      console.log({...s,tipo:0})
+    }
+    handleSolicitudes()
+  }
   
-  const handleDeleteSolicitud = (solicitudToDelete) => {
+  const botonOjo = (s) => {
+    //SolicitudApi.actualizar
+    console.log({...s,tipo:1})
+    handleSolicitudes()
+  }
+
+  const botonX = (s) => {
+    //SolicitudApi.borrar(s._id)
+    handleSolicitudes()
+
     /*
     const token = localStorage.getItem("token");
 
@@ -152,7 +104,64 @@ const Solicitudes = () => {
         console.error("Error al eliminar la solicitud:", error);
       });
       */
-  };
+  }
+  
+  const handleSolicitudes = () => {
+    
+    // Utiliza la función SolicitudesRecibidasUsuario de tu API de solicitudes
+    SolicitudApi.SolicitudesRecibidasUsuario(window.localStorage.token)
+    .then((response) => {
+      console.log("resp",response)
+      if (response && response.data) {
+        
+        //const solicitudesData = response.data.solicitudesRecibidas;
+        //setSolicitudes(solicitudesData);
+        setSolicitudes([
+          {...defaultSolicitud,_id:'0',idEmisor:'1',idReceptor:'0',tipo:0, ano:2023,mes:11,dia:27,diaSem:"Lunes",hora:2,minuto:45},
+          {...defaultSolicitud,_id:'1',idEmisor:'2',idReceptor:'0',tipo:0, ano:2023,mes:11,dia:26,diaSem:"Lunes",hora:2,minuto:45},
+          {...defaultSolicitud,_id:'2',idEmisor:'0',idReceptor:'3',tipo:0, ano:2023,mes:11,dia:25,diaSem:"Lunes",hora:2,minuto:45},
+          {...defaultSolicitud,_id:'3',idEmisor:'0',idReceptor:'4',tipo:1, ano:2023,mes:11,dia:10,diaSem:"Lunes",hora:2,minuto:45},
+        ]);
+        
+      }
+    })
+    .catch((error) => {
+      console.error("Error al obtener las solicitudes:", error);
+    });
+
+    // Llama a la función para actualizar el campo viewer
+    SolicitudApi.actualizarViewerSolicitudes(window.localStorage.token)
+    .then((response) => {
+      if (response && response.data) {
+        console.log(response.data.message);
+      }
+    })
+    .catch((error) => {
+      console.error("Error al actualizar el viewer:", error);
+    });
+  }
+  
+  useEffect(() => {
+
+    //UsuarioApi.findAll().then((users) => {
+    //const aux = user.data.usuarios;
+    //setUsuarios(aux)
+    setUsuarios([
+      {...defaultUsuario,_id:'0',nombre:'Yo',apodo:'YoApodo',mostrar_nombre:true},
+      {...defaultUsuario,_id:'1',nombre:'Adrián',apodo:'AdriánApodo',mostrar_nombre:true},
+      {...defaultUsuario,_id:'2',nombre:'RodrigoNombre',apodo:'Rodrigo',mostrar_nombre:false},
+      {...defaultUsuario,_id:'3',nombre:'Camayo',apodo:'CamayoApodo',mostrar_nombre:true},
+      {...defaultUsuario,_id:'4',nombre:'George',apodo:'GeorgeApodo',mostrar_nombre:true}
+    ])
+
+    //UsuarioApi.findCurrent().then((user) => {
+    //const aux = user.data.usuario;
+    //setUsuario(aux)
+    setUsuario({...defaultUsuario,_id:'0',nombre:'Yo',apodo:'YoApodo',mostrar_nombre:true})
+
+    handleSolicitudes()
+    
+  }, []);
 
   return (
     <div id='container'>
@@ -206,22 +215,21 @@ const Solicitudes = () => {
             <>
               <div className={styles.Caja} style={{top: `${primero+salto*index}px`}} onClick={e => location.pathname = `/perfil?id=${encodeURIComponent(s[val])}`}/>
               <div className={styles.Elipse} style={{top: `${primero-7+salto*index}px`}} onClick={e => location.pathname = `/perfil?id=${encodeURIComponent(s[val])}`}/>
+              <div className={styles.contlax} style={{top: `${primero-4+salto*index}px`}} onClick={e => botonAmarillo()}/>
               {
                 pag===2 ?
                   <>
-                    <div className={styles.contlax} style={{top: `${primero-1+salto*index}px`}} onClick={e => onDeleteSolicitud(s)}/>
                     <img className={styles.lax} style={{top: `${primero+10+salto*index}px`}} alt="" src="/group-180.svg" />
-                    <img className={styles.ojoMa} style={{top: `${primero-1+salto*index}px`}} alt="" src="/vector2.svg" />
+                    <img className={styles.ojoMa} style={{top: `${primero-1+salto*index}px`}} onClick={e => botonOjo(s)} alt="" src="/vector2.svg" />
                     <img className={styles.ojo} style={{top: `${primero+10+salto*index}px`}} alt="" src="/frame1.svg" />
                   </>
                 :
                   <>
-                    <div className={styles.solicitudes3Child11} style={{top: `${primero-4+salto*index}px`}}/>
-                    <img className={styles.vectorIconz} style={{top: `${primero+3+salto*index}px`}} alt="" src="/vector2.svg" />
+                    <img className={styles.vectorIconz} style={{top: `${primero+3+salto*index}px`}} onClick={e => botonX()} alt="" src="/vector2.svg" />
                     <img className={styles.solicitudes3Child14} style={{top: `${primero+17+salto*index}px`}} alt="" src="/group-180.svg" />
                     {
                       pag===1 ?
-                        null
+                        <img className={styles.checkIcon} style={{top: `${primero+18+salto*index}px`}} alt="" src="/check.svg" />
                       :
                         <img className={styles.vectorIcon5z} style={{top: `${primero+15+salto*index}px`}} alt="" src="/vector6.svg" />
                     }
